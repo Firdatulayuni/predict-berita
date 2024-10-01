44b80117edd9589676dfdf1499836696cff78896
Firdatul A'yuni <107979861+Firdatulayuni@users.noreply.github.com>
2024-10-01 12:05:02 +0700

    Update predict_berita.py

--- predict_berita.py
+++ predict_berita.py
@@ -8,6 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
+import os
 
 # Tambahkan path stopwords manual
 stopwords_path = 'nltk_data/corpora'
